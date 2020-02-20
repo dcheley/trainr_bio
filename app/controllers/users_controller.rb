0f734aa97bc3cd2c1fa4982da205dbac8bb88ace
dcheley <danielcheley@hotmail.com>
2020-02-20 18:37:18 -0500

    Creating and styling several navbar and footer cpartials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "Welcome!"
@@ -48,6 +48,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  def home
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  root to: 'users#home'
 
   resources :users
   resources :studios
-  resources :events, only: [:new, :create, :update, :destroy]
+  resources :events
 
   get 'login' => 'sessions#new', as: :login
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy', as: :logout
+
+  get 'home' => 'users#home', as: :home
 end
