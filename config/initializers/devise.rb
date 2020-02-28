65fa790b11a0b306748b93e90ef9090fbb0ce239
dcheley <danielcheley@hotmail.com>
2020-02-27 19:08:40 -0500

    Adding host url and updating config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'dc94cf404e015d0690c8eebe19d766eb7a15203c31d5eb4f7381c64634648ace0ed28e68986be01ad2a1e7b8d006fde7efc7f86c2fa4dcb71702d173f690ec15'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = Rails.application.secrets.GMAIL_USERNAME
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
