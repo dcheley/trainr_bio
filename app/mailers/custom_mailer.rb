ac24e5e1baeb9303b4a6b579a6efb7ba6dd01fc6
dcheley <danielcheley@hotmail.com>
2020-03-03 13:39:15 -0500

    Testing different setup

--- app/mailers/custom_mailer.rb
+++ app/mailers/custom_mailer.rb
@@ -11,11 +11,11 @@ class CustomMailer < Devise::Mailer
     from = SendGrid::Email.new(email: 'trainrbio@gmail.com')
     to = SendGrid::Email.new(email: @user.email)
     subject = "Click the confirmation link to verify your Trainrbio account."
-    content = SendGrid::Content.new(type: 'text/plain', value: "https://trainrbio.herokuapp.com/users/confirmation?confirmation_token=#{@token}")
+    content = SendGrid::Content.new(type: 'text/plain', value: "#{root_url}/users/confirmation?confirmation_token=#{@token}")
     mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    super
+    # super
   end
 end
