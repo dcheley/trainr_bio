3d06f66b940ebc46d7c4b08a585669bba176e54b
dcheley <danielcheley@hotmail.com>
2020-03-04 18:49:50 -0500

    Updating url

--- app/mailers/custom_mailer.rb
+++ app/mailers/custom_mailer.rb
@@ -11,7 +11,7 @@ class CustomMailer < Devise::Mailer
     from = SendGrid::Email.new(email: 'trainrbio@gmail.com')
     to = SendGrid::Email.new(email: @user.email)
     subject = "Click the confirmation link to verify your Trainrbio account."
-    content = SendGrid::Content.new(type: 'text/plain', value: "https://trainr.bio/users/confirmation?confirmation_token=#{@token}")
+    content = SendGrid::Content.new(type: 'text/plain', value: "https://www.trainr.bio/users/confirmation?confirmation_token=#{@token}")
     mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
