45ea41b6139bbaeed1c764a7477c6d6a6e660092
mastrella <michael_mastrella@hotmail.com>
2020-04-04 20:07:02 -0400

    edit bio update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,8 +86,10 @@ class UsersController < ApplicationController
 
   def edit_bio
     @user = current_user
-    @milestone = Milestone.new
+    @mypractices = UserPracticeCategory.where(user_id: @user.id).includes(:practice_category).sort_by { |p| p.practice_category.name }
+    @myspecialties = UserSpecialtyCategory.where(user_id: @user.id).includes(:specialty_category).sort_by { |s| s.specialty_category.name }
     @milestones = Milestone.all.order("year ASC")
+    @milestone = Milestone.new
     @practices = PracticeCategory.all.order("name ASC")
     @specialties = SpecialtyCategory.all.order("name ASC")
   end
