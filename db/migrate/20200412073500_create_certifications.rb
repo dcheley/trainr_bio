ea6c9ae630e362dfd58185f6a43aaff138df5bcd
mastrella <michael_mastrella@hotmail.com>
2020-04-12 03:46:49 -0400

    adding migration to create certifications and rename instragram_url on user table

---
+++ db/migrate/20200412073500_create_certifications.rb
@@ -0,0 +1,16 @@
+class CreateCertifications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :certifications do |t|
+	t.integer :trainer_id, index: true
+	t.string :name
+	t.string :level
+	t.string :accreditor
+	t.string :website_url
+	t.string :description
+	t.date :start_date
+	t.date :end_date
+    end
+
+	rename_column :users, :instragram_url, :instagram_url
+  end
+end
