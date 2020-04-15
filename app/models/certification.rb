f81c55b63ad7f06129c97e6738dae69df196efad
mastrella <michael_mastrella@hotmail.com>
2020-04-14 21:57:21 -0400

    trying to get certifications to save properly

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,7 +1,7 @@
 class Certification < ApplicationRecord
-  has_one_attached :cert_pic
-
   validates :certification, :trainer_id, :name, :level, :accreditor, :website_url, :description, :start_date, :end_date, presence: true
+
+  has_one_attached :cert_pic
 
   belongs_to :trainer, foreign_key: 'trainer_id', class_name: 'User'
 end
